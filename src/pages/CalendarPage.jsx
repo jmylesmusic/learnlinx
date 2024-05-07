9de8da52c6ef9df5491548324fed2d4cae0f7ea4
James Myles <myles.james@gmail.com>
2024-05-07 18:03:46 +0200

    Event Table added to Calendar

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,8 +1,10 @@
 import LargeCalendarComponent from "../components/LargeCalendarComponent";
+import EventTable from "../components/EventTable";
 const CalendarPage = () => {
   return (
     <div>
       <LargeCalendarComponent />
+      <EventTable />
     </div>
   );
 };
