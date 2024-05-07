b8ef06139ee1906dc4f9359a01e16b32ea1bcd4e
Fabio Niglio <fabio_niglio@hotmail.com>
2024-05-07 17:13:41 +0200

    daily react

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,10 @@
 import CurrentCourses from "../components/CurrentCourses";
 import { useState, useEffect } from "react";
 import UpcomingCourses from "../components/UpcomingCourses";
+// import DailyIframe from "@daily-co/daily-js";
+// import BrowserUnsupported from "../components/BrowserUnsupported/BrowserUnsupported";
+// import VideoCall from "../components/VideoCall/VideoCall";
+import { DailyProvider, useCallObject } from "@daily-co/daily-react";
 const API_URL = import.meta.env.VITE_API_URL;
 const Dashboard = () => {
   return (
@@ -9,9 +13,11 @@ const Dashboard = () => {
       <h3>Current courses</h3>
       <CurrentCourses />
       <br />
+      {/* DailyIframe.supportedBrowser().supported ? <VideoCall /> :
+      <BrowserUnsupported /> */}
       <br />
       <h3>Upcoming courses:</h3>
-      <UpcomingCourses/>
+      <UpcomingCourses />
     </>
   );
 };
