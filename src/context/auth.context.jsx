d99111100f136f20f057626ad52af696eece3cd9
James Myles <myles.james@gmail.com>
2024-05-07 09:41:39 +0200

    Updated Routes

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-const API_URL = "http://localhost:5005";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const AuthContext = React.createContext();
 
