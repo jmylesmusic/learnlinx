042cfc23850d4a7831078690ea617415eee49698
James Myles <myles.james@gmail.com>
2024-05-08 17:33:04 +0200

    Event Table colors

--- src/components/EventTable.jsx
+++ src/components/EventTable.jsx
@@ -1,8 +1,9 @@
 import cx from "clsx";
 import { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../context/auth.context.jsx";
-import { Table, ScrollArea } from "@mantine/core";
+import { Table, ScrollArea, useMantineTheme } from "@mantine/core";
 import classes from "../styles/TableScrollArea.module.css";
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 export function EventTable() {
@@ -11,6 +12,7 @@ export function EventTable() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const theme = useMantineTheme();
 
   useEffect(() => {
     const storedToken = localStorage.getItem("authToken");
@@ -53,40 +55,25 @@ export function EventTable() {
     return `${year}-${month}-${day}`;
   };
 
-  // Function to sort data by date and time
-  const sortEventsByDateAndTime = (data) => {
-    return data.sort((a, b) => {
-      // First compare the dates
-      const dateComparison = new Date(a.date) - new Date(b.date);
-      if (dateComparison !== 0) {
-        return dateComparison;
-      }
-      // If dates are the same, compare times
-      const timeA = a.timeStart.split(":").map(Number); // Convert "hh:mm" to [hh, mm]
-      const timeB = b.timeStart.split(":").map(Number);
-      // Compare hours first, then minutes if hours are the same
-      return timeA[0] - timeB[0] || timeA[1] - timeB[1];
-    });
-  };
-
-  // Filtering events that are today or earlier
-  const filterPastEvents = (data) => {
-    const today = new Date();
-    today.setHours(0, 0, 0, 0); // Reset time to 00:00:00 to compare only dates
-    return data.filter((item) => new Date(item.date) >= today);
-  };
-
-  // Sorting and filtering the data
-  const sortedAndFilteredData = sortEventsByDateAndTime(filterPastEvents(data));
-
-  // Then map over the sorted and filtered data to create table rows
-  const rows = sortedAndFilteredData.map((row) => (
-    <Table.Tr key={row._id}>
-      <Table.Td>{formatDate(row.date)}</Table.Td>
-      <Table.Td>{row.timeStart}</Table.Td>
-      <Table.Td>{row.eventTitle}</Table.Td>
-    </Table.Tr>
-  ));
+  const rows = data.map((event) => {
+    const colorKey =
+      event.color in theme.colors
+        ? theme.colors[event.color][1]
+        : theme.colors.red[1];
+    return (
+      <Table.Tr key={event._id}>
+        <Table.Td style={{ backgroundColor: colorKey }}>
+          {formatDate(event.date)}
+        </Table.Td>
+        <Table.Td style={{ backgroundColor: colorKey }}>
+          {event.timeStart}
+        </Table.Td>
+        <Table.Td style={{ backgroundColor: colorKey }}>
+          {event.eventTitle}
+        </Table.Td>
+      </Table.Tr>
+    );
+  });
 
   return (
     <ScrollArea
